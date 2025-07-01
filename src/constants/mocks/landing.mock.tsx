2697d63816057509593a284cdbaee2e0afb57412
morgan segura <morgansegura@gmail.com>
2025-06-30 21:54:44 -0700

    feat: updates projects schema

--- src/constants/mocks/landing.mock.tsx
+++ src/constants/mocks/landing.mock.tsx
@@ -1,54 +1,14 @@
-import { ButtonProps } from "@/components/ui/button/button";
-import type {
-  ImageType,
-  LinkType,
-  SpacingType,
-  StringOrArrayOfStringsType,
-} from "@/types/content.types";
+import type { SpacingType } from "@/types/content.types";
 
 export interface LandingPageProps {
   __id: string;
   __sectionId: string;
   sectionClassName?: string;
   sectionTitle?: string;
-  quote?: string;
   sectionTheme?: string;
   buffer?: SpacingType;
-  subheading?: StringOrArrayOfStringsType;
-  heading?: StringOrArrayOfStringsType;
-  kicker?: string;
-  description?: StringOrArrayOfStringsType;
-  link?: ButtonProps;
-  works?: {
-    __id: string;
-    kicker?: string;
-    description?: StringOrArrayOfStringsType;
-    heading?: string;
-    image?: ImageType;
-    link?: ButtonProps;
-  }[];
-  projects?: {
-    __id: string;
-    kicker?: string;
-    description?: StringOrArrayOfStringsType;
-    heading?: string;
-    cards?: {
-      __id: string;
-      description?: StringOrArrayOfStringsType;
-      heading?: string;
-      image?: ImageType;
-      link?: ButtonProps;
-    }[];
-  }[];
   spacer?: boolean;
-  mediaLinks?: ButtonProps[];
-  intro?: string;
 }
-
-const workImage = {
-  width: 750,
-  height: 900,
-};
 
 export const LANDING_PAGE_MOCK: LandingPageProps[] = [
   {
@@ -90,60 +50,5 @@ export const LANDING_PAGE_MOCK: LandingPageProps[] = [
     sectionClassName: "projects-section",
     sectionTitle: "Projects",
     buffer: { top: true, bottom: true },
-    heading: "Project & Pro Bono Work",
-    description: [
-      "In addition to professional work, I regularly take on pro bono projects to support my community and causes I believe in.",
-      "I built a Single Page Application for author LeSean Johnson’s Nations of Stone trilogy, redesigned the official website for local soccer club Chula Vista FC (currently in progress), and continue to lead web development and marketing for a nonprofit my wife and I started, Grassroots Foundation, which empowers youth through access to elite-level training and opportunity.",
-    ],
-    projects: [
-      {
-        __id: "0",
-        heading: "",
-        description: "",
-        cards: [
-          {
-            __id: "0",
-            heading: "Project: Nations of Stone Trilog",
-            description:
-              " Created a Single Page Application (SPA) for my good friend author LeSean Johonson, author of the incredible Nations of Stone Trilogy. He wanted a Wordpress website built from scratch and needed it in a hurry. I was able to create the SPA in a couple of days, and get it up in time for Book One's release.",
-            image: {
-              src: "/images/projects-alj.jpg",
-              alt: "Author LeSean Johnson",
-              width: 650,
-              height: 300,
-            },
-            link: {
-              href: "https://authorlj.com/",
-              children: "Official Website",
-              target: "_blank",
-            },
-          },
-        ],
-      },
-      {
-        __id: "1",
-        heading: "",
-        description: "",
-        cards: [
-          {
-            __id: "0",
-            heading: "Project: Nations of Stone Trilog",
-            description:
-              " Created a Single Page Application (SPA) for my good friend author LeSean Johonson, author of the incredible Nations of Stone Trilogy. He wanted a Wordpress website built from scratch and needed it in a hurry. I was able to create the SPA in a couple of days, and get it up in time for Book One's release.",
-            image: {
-              src: "/images/projects-alj.jpg",
-              alt: "Author LeSean Johnson",
-              width: 650,
-              height: 300,
-            },
-            link: {
-              href: "https://authorlj.com/",
-              children: "Official Website",
-              target: "_blank",
-            },
-          },
-        ],
-      },
-    ],
   },
 ];
---
+++ src/constants/mocks/projects-block.mock.tsx
@@ -0,0 +1,66 @@
+const imageSizes = {
+  width: 650,
+  height: 300,
+};
+export const PROJECTS_BLOCK_MOCK = {
+  heading: "Project & Pro Bono Work",
+  description: [
+    "In addition to professional work, I regularly take on pro bono projects to support my community and causes I believe in.",
+    "I built a Single Page Application for author LeSean Johnson’s Nations of Stone trilogy, redesigned the official website for local soccer club Chula Vista FC (currently in progress), and continue to lead web development and marketing for a nonprofit my wife and I started, Grassroots Foundation, which empowers youth through access to elite-level training and opportunity.",
+  ],
+  cards: [
+    {
+      __id: "0",
+      heading: "Project: Nations of Stone Trilog",
+      description:
+        " Created a Single Page Application (SPA) for my good friend author LeSean Johonson, author of the incredible Nations of Stone Trilogy. He wanted a Wordpress website built from scratch and needed it in a hurry. I was able to create the SPA in a couple of days, and get it up in time for Book One's release.",
+      image: {
+        src: "/images/projects-alj.jpg",
+        alt: "Author LeSean Johnson",
+        width: imageSizes.width,
+        height: imageSizes.height,
+      },
+      link: {
+        href: "https://www.authorlj.com/",
+        children: "Official Website",
+        target: "_blank",
+      },
+    },
+    {
+      __id: "1",
+      heading: "Project: Chula Vista Fúbol Club",
+      description:
+        "Pro bono web reconstruction. New S.E.O and marketing campaign, website refresh and architecture overhaul. This is currently a Wordpress website, however I am nearly finished creating a custom CMS built with Nest.js and Next.js to replace it.",
+      tag: "Coming Soon",
+      image: {
+        src: "/images/projects-alj.jpg",
+        alt: "Chula Vist FC",
+        width: imageSizes.width,
+        height: imageSizes.height,
+      },
+      link: {
+        href: "https://www.chulavistafc.com",
+        children: "Old Website",
+        target: "_blank",
+      },
+    },
+    {
+      __id: "2",
+      heading: "Project: Grass Roots Foundation",
+      description:
+        'A work inprogress. Grass Roots Foundation is a Nonprofit my wife and I started to help youth soccer players in underserved communities get access to fields, equipment and elite soccer training. At the moment there is a landing page, however I will be build a website with all of the "bells and whistles."',
+      tag: "Work in Progress",
+      image: {
+        src: "/images/projects-grf.jpg",
+        alt: "Grassroots Foundation",
+        width: imageSizes.width,
+        height: imageSizes.height,
+      },
+      link: {
+        href: "https://www.grassrootsfdn.org",
+        children: "Current Landing Page",
+        target: "_blank",
+      },
+    },
+  ],
+};
